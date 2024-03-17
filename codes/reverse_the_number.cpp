9f96c9a2820d4213a739e2a054963a72421db63a
sniffsneaker <149600651+sniffsneaker@users.noreply.github.com>
2024-03-17 11:34:49 +0530

    reverse_the_number.cpp

--- codes/reverse_the_number.cpp
+++ codes/reverse_the_number.cpp
@@ -1,4 +1,4 @@
-// this program is to reverse a number  ex : 1080 reverse is 801 
+// this program is to reverse a number ... ex : 1080 reverse is 801 
 
 #include <bits/stdc++.h>
 using namespace std ;
@@ -7,19 +7,28 @@ int main()
 {
     // this program is to find the number of digits in a number 
     int n ,count = 0 ,reversenum = 0; 
+    cout << "Enter your input integer number : " ;
     cin >> n ; 
+    int a = n ;
     int numberofdigits = log10(n) + 1 ;
-    cout << numberofdigits << endl ; 
+    cout <<"The number of digits in the given number is " << numberofdigits << endl ; 
     while(n>0) 
     {
      int lastdigit = n%10 ;
-     cout << lastdigit ; 
      reversenum = lastdigit*pow(10,numberofdigits-1)  + reversenum ; 
      numberofdigits-- ; 
      n = n/10 ;
     }
     
-    cout << "\n" << reversenum ; 
-    
+    cout << "\nThe reverse number of given number is " << reversenum ; 
+    if(reversenum == a) 
+    {
+        cout << "\nThe given number is " << a <<" is a palindrome" ;
+    }
+    else
+    {
+        cout << "\nThe given number is " << a <<" is not a palindrome" ;
+    }
     return 0;
 }
+
